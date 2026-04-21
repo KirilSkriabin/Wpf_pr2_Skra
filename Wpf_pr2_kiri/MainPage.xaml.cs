8e0d19cb6a9f270df42ccffc95afad0938198d76
Kiril <c.skriabin.kyrylo@student.uzhnu.edu.ua>
2026-04-21 17:39:24 +0300

    Add project files.

---
+++ Wpf_pr2_kiri/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_pr2_kiri
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();  
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BtnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new SettingsPage());
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Wpf_pr2_kiri/SettingsPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_pr2_kiri
+{
+    /// <summary>
+    /// Interaction logic for SettingsPage.xaml
+    /// </summary>
+    public partial class SettingsPage : Page
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboLang_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboLang.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string lang = selectedItem.Tag.ToString();
+                ChangeLanguage(lang);
+            }
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void ChangeLanguage(string lang)
+        {
+            ResourceDictionary dict = new ResourceDictionary();
+
+            // Шлях до файлів. Переконайся, що папка Resources називається саме так!
+            dict.Source = new Uri($"/Resources/Lang.{lang}.xaml", UriKind.Relative);
+
+            // Видаляємо стару мову і додаємо нову
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(dict);
+        }   
+    }
+}
